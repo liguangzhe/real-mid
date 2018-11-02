cc2548d0cd1547bc501cf35499fc15dc1e1e3d54
lgz0012 <gl279@njit.edu>
2018-11-02 00:04:22 -0400

    i wrote a login feature test and it returned a 200 status code

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,12 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testLoginPage(){
+
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
 }
