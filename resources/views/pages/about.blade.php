4971dac782d12e3a325ce4e1615059291e5bbac0
lgz0012 <gl279@njit.edu>
2018-11-01 23:19:28 -0400

    i made a pagescontroller and added the about page into my main menu

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,1 @@
+<h1>welcome to my about page</h1>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,6 +71,7 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('about') }}">about</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
