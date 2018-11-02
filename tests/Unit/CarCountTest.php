4a4c337a294327146965817615d5fe80069607df
lgz0012 <gl279@njit.edu>
2018-11-02 02:54:45 -0400

    i created a unit test to count the number insert into car table and it was succeeded

---
+++ tests/Unit/CarCountTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class CarCountTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+
+        {
+            $carCount = 50;
+            $this->assertTrue(true);
+        }
+
+}
