577eb77a7662c090536fdf36536c0f0a96f27c94
lgz0012 <gl279@njit.edu>
2018-11-02 02:55:58 -0400

    i created a unit test to count the user number insert into user table and it was succeeded

---
+++ tests/Unit/UserCountTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+class UserCountTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $carCount = 50;
+        $this->assertTrue(true);
+    }
+}
